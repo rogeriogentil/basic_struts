99359759e5e66cd4ba0d56cf61ca94fea85bdff8
CIANDT\rogeriogentil <rogeriogentil@ciandt.com>
2021-11-08 18:06:03 -0300

    Add validation to registration form

--- src/main/java/rogeriogentil/controller/register/RegisterAction.java
+++ src/main/java/rogeriogentil/controller/register/RegisterAction.java
@@ -6,6 +6,7 @@ import rogeriogentil.model.register.Person;
 
 public class RegisterAction extends ActionSupport {
 
+	private static final long serialVersionUID = 1874531246847L;
 	private Person personBean;
 	
 	public String execute() throws Exception {
@@ -20,4 +21,15 @@ public class RegisterAction extends ActionSupport {
 		this.personBean = personBean;
 	}
 		
+	public void validate() {
+		if (personBean.getFirstName().length() == 0) {
+			addFieldError("personBean.firstName", "First name is required.");
+		}
+		if (personBean.getEmail().length() == 0) {
+			addFieldError("personBean.email", "Email is required.");
+		}
+		if (personBean.getAge() < 18) {
+			addFieldError("personBean.age", "Age is required and must be 18 or older");
+		}
+	}
 }
